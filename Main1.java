7664993ae789941f9d46d8a153f4b8296bc36e48
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-11-05 19:21:05 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Main1{
+public static void main(String[]args){
+
+System.out.println(Thread.currentThread().getName()); 
+}
+}
---
+++ Ppp2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Ppp2 extends Thread{
+    public static void main(String [] args)
+    {   System.out.println("kitty");
+        System.out.println(Thread.currentThread().getPriority());
+    }
+}
